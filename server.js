03e65eb1e89f5c94d4421e1d43bb3b9a5960eef6
JuanGoez18 <160356853+JuanGoez18@users.noreply.github.com>
2024-11-06 16:56:09 -0500

    administrar plato

--- server.js
+++ server.js
@@ -18,22 +18,24 @@ app.get('/', (req, res) => {
 const svcPedirMesa = require('./servicios/svc-pedir_mesa');
 const svcEnviarpedido = require('./servicios/svc-enviar_registro');
 const svcPanelControl = require('./servicios/svc-panel_control-1');
-const svcverificarPermiso = require('./servicios/svc-verificar_permiso');
-const svceleminarSolicitud = require('./servicios/svc-eliminar_solicitud');
-const svcpedirPlato = require('./servicios/svc-pedir_plato');
-const svcguardarPedido = require('./servicios/svc-guardar_pedido');
-const svcobtenerPedidos = require('./servicios/svc-obtener_pedido');
+const svcVerificarPermiso = require('./servicios/svc-verificar_permiso');
+const svcEleminarSolicitud = require('./servicios/svc-eliminar_solicitud');
+const svcPedirPlato = require('./servicios/svc-pedir_plato');
+const svcGuardarPedido = require('./servicios/svc-guardar_pedido');
+const svcObtenerPedidos = require('./servicios/svc-obtener_pedido');
+const svcAdministrarplatos = require('./servicios/svc-administrar_platos');
 
 
 // Ruta de los servicios
 app.use('/servicios/pedir-mesa', svcPedirMesa);
 app.use('/servicios/enviar-registro', svcEnviarpedido);
 app.use('/servicios/panel-control', svcPanelControl);
-app.use('/servicios/verificar-permiso', svcverificarPermiso);
-app.use('/servicios/eliminar-solicitud', svceleminarSolicitud);
-app.use('/servicios/pedir-plato', svcpedirPlato);
-app.use('/servicios/guardar-pedido', svcguardarPedido);
-app.use('/servicios/obtener-pedidos', svcobtenerPedidos);
+app.use('/servicios/verificar-permiso', svcVerificarPermiso);
+app.use('/servicios/eliminar-solicitud', svcEleminarSolicitud);
+app.use('/servicios/pedir-plato', svcPedirPlato);
+app.use('/servicios/guardar-pedido', svcGuardarPedido);
+app.use('/servicios/obtener-pedidos', svcObtenerPedidos);
+app.use('/servicios/administrar-plato', svcAdministrarplatos);
 
 // Iniciar el servidor
 app.listen(PORT, () => {
