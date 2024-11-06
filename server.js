159d3b3445b493b7acd98738e06c3f90b32400f0
JuanGoez18 <160356853+JuanGoez18@users.noreply.github.com>
2024-11-06 13:51:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ app.get('/', (req, res) => {
     res.send('Bienvenido al servidor');
 });
 
+// Importar el archivo de servicio
+const svcPedirMesa = require('./servicios/svc-pedir-mesa');
+
 // Ruta de los servicios
 app.get('/servicios/pedir-mesa', (req, res) => {
-    res.json({ message: "Aquí están los datos de la mesa" });
+    res.json(svcPedirMesa);  // Envía los datos del servicio
 });
 
 // Iniciar el servidor
