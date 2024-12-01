dfb291d90f28971cf78e684dc62045fab46257e4
JuanGoez18 <160356853+JuanGoez18@users.noreply.github.com>
2024-12-01 16:21:22 -0500

    paginacion facturas

--- servicios/svc-obtener_facturas.js
+++ servicios/svc-obtener_facturas.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const conexion = require('../dbConnection');
 
 router.get('/', (req, res) => {
+
+    const offset = parseInt(req.query.offset) || 0;
+    const limit = parseInt(req.query.limit) || 10;
+
     const sql = 'SELECT pago_total, fecha, nombre_mesa, nombre_usuario, cc, numero FROM factura';
     
     conexion.query(sql, (err, resultados) => {
@@ -11,6 +15,18 @@ router.get('/', (req, res) => {
             res.status(500).send("Error al obtener las facturas.");
         } else {
             res.json(resultados);
+        }
+    });
+});
+
+router.get('/total', (req, res) => {
+    const sql = 'SELECT COUNT(*) AS total FROM factura';
+    conexion.query(sql, (err, resultados) => {
+        if (err) {
+            console.error("Error al obtener el total de facturas:", err);
+            res.status(500).send("Error al obtener el total de facturas");
+        } else {
+            res.json({ total: resultados[0].total });
         }
     });
 });
